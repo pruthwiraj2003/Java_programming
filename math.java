d3bdc7d24ba67203b458f0882a0bcddede9f02f1
Pruthwiraj mahapatra <pruthwirajmahapatra2@gmail.com>
2025-02-11 22:15:52 +0530

    Calculate the distance of the line

---
+++ math.java
@@ -0,0 +1,128 @@
+
+import java .util.Scanner;
+
+
+public class math {
+public static void main(String[] args) {
+
+    System.out.println();
+    Scanner sc = new Scanner(System.in);
+    // double x1= 1, x2=2 ,x3=3, y1=1, y2=2, y3=3;
+
+
+    // double m1=(y2-y1) / (x2-x1);   
+    // double m2=  (y3-y2)/(x3-x2);
+    // if(m1==m2){
+    //     System.out.println(" the line are lie in a single ");
+
+    // }
+    // else{
+    //     System.out.println("The line do not lia in a line ");
+    // }
+    //WAP to foind out if it lies on the x -axis ,y- axis or at origin,viz(0,0)
+//     System.out.print("Enter the value of x :  ");
+//     int x= sc.nextInt();
+//     System.out.print("Enterv the  value of y : ");
+//     int y = sc.nextInt();
+//     if (y==0){
+//         System.out.println("The point lia on  the x axis");
+//       }
+
+
+//   else if (x==0){
+//     System.out.println("The point lia on  the y axis");
+//   }
+//   else if (x==0 && y==0){
+//     System.out.println("The point lies on the origine");
+//   }
+//   else{
+   // System.out.println("The point liesa between x and y axis");
+
+    // Given the co ordinates (x,y) of a center of a circle and its radius, write a program which will determine whether a point lies inside the circle,on the circle or out side the circle 
+   //   System.out.println("Enter the value of x");
+   //   int x= sc.nextInt();
+   //   System.out.println("Enter the value of y ");
+   //   int y = sc. nextInt();
+   //   System.out.println("Enter the value of R");
+   //   int R = sc.nextInt();
+   //        R=R*R;
+   //        System.out.println(R);
+   //   System.out.println("Enter the value of x1");
+   //   int x1= sc.nextInt();
+   //   System.out.println("Enter the value of y1");
+   //   int y1 = sc.nextInt();
+   //   int m = (x-x1)*(x-x1)+(y-y1)*(y-y1);
+   //   System.out.println(m);
+   //   if(m>R){
+   //      System.out.println("thge point lies out side of the circle ");
+   //   }
+   //   else if (m<R){ 
+   //      System.out.println("The lies inside the circle ");
+
+   //   }
+
+   //   else {
+   //      System.out.println("The point lies on the circle ");
+
+   //   }
+     
+
+   // int a = 300,b,c;
+   // if(a>=400)
+   //  b=300;
+   //  c=200;
+   //  System.out.println(b+""+c);
+
+
+   //   int x =3,y=5;
+   //  if (x==3)
+   //  System.out.println(x);
+   //  else;
+   //  System.out.println(y);
+    
+   // int x =3;
+   // double y = 3.0;
+   // if(x==y)
+   //  System.out.println("x and y are equal ");
+   //  else
+   //  System.out.println("x and y are not equal");
+
+//    int x = ,y,z;
+//   // y=x=10;
+//    boolean z= x < 10;
+//    System.out.println("x="+x+"y="+y+"z="+z);
+   
+    
+   //  int a = 5,b;
+   //  boolean c;
+
+   //  b=a=15;
+   //  c=a<15;
+   // int k = 35;
+   // System.out.println(k==35);
+   // System.out.println(k=50);
+   // System.out.println(k>40);
+   // int x = 15;
+   // System.out.println(x!=15);
+   // System.out.println(x=20);
+   // System.out.println(x<30);
+   // int i = 65;
+   // char j= 'A';
+   // if (i==j)
+   // System.out.println("C is WOW");
+   // else 
+   // System.out.println(" C is headach ");
+
+
+   
+   int a = 500,b,c;
+   if(a>=400)
+   b = 300;
+   c=200;
+   System.out.println(b);
+   System.out.println(c);
+
+
+}
+
+}
